82759945f43d2ff8c8878ae2d27d55fa1480e3cd
suzukikoutarou <kotarosz727@gmail.com>
2023-02-07 09:00:17 +0900

    Promises example

--- promises.ts
+++ promises.ts
@@ -15,3 +15,41 @@ fs.promises.readFile("./test1.txt")
     .catch((error) => {
         console.log(`an error occurred : ${error}`);
     });
+
+function delayedPromise(): Promise<void> {
+    // return new Promise object
+    return new Promise<void>
+    ( // start constructor
+        (
+            resolve: () => void, // resolve function
+            reject: () => void   // reject function
+        ) => {
+            // start of function definition
+            function afterTimeout() {
+                resolve();
+            }
+            setTimeout(afterTimeout, 1000);
+            // end of function definition
+        }
+    ); // end constructor
+}
+
+delayedPromise().then(() => {
+    console.log(`delayed promise returned`);
+});
+
+function errorPromise(): Promise<void> {
+    return new Promise<void>(
+        (   // constructor
+            resolve: () => void,
+            reject: () => void
+        ) => {
+            // function definition
+            console.log(`2. calling reject()`);
+            reject();
+        }
+    )
+}
+console.log(`1. calling errorPromise()`);
+errorPromise().then(() => { })
+    .catch(() => { console.log(`3. caught an error`) });
