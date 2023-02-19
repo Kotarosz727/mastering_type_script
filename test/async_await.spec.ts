59279ac11d1d87b50bec0c47cae07b7f24cb5991
suzukikoutarou <kotarosz727@gmail.com>
2023-02-20 08:42:41 +0900

    feat: add jest async await test

---
+++ test/async_await.spec.ts
@@ -0,0 +1,23 @@
+class AsyncWithPromise {
+    delayedPromise(): Promise<string> {
+        return new Promise<string>(
+            (resolve: (str: string) => void,
+             reject: (str: string) => void) => {
+                setTimeout(() => {
+                    console.log(`2. returning success`);
+                    resolve("success");
+                }, 1000)
+            }
+        )
+    }
+}
+
+describe("async test with await", () => {
+    let asyncWithPromise = new AsyncWithPromise();
+    it("should return success", async () => {
+        console.log(`1. calling delayedPromise`);
+        let result = await asyncWithPromise.delayedPromise();
+        console.log(`3. expecting result`);
+        expect(result).toBe("success");
+    })
+});
